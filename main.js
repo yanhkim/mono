12af51c1f375351f5c5542e44c1a95d412dbd15c
Namhoon Kim <nh42.kim@samsung.com>
2012-12-14 15:49:27 +0900

    implement static log

---
+++ main.js
@@ -0,0 +1,3 @@
+require(['mono'], function(Mono) {
+    window.Mono = Mono;
+});
---
+++ mono.js
@@ -0,0 +1,51 @@
+define(function() {
+    function timetag() {
+        // format: Fri Dec 14 2012 15:02:39 GMT+0900 (KST)
+        var d = new Date(),
+            ds = d.toLocaleString(),
+            re = /\w+ (\w+ \d+ \d+ \S+)/,
+            time = re.exec(ds)[1],
+            mil = d.getMilliseconds();
+
+        return time + ',' + mil;
+    }
+
+    function logger(domain) {
+        domain = domain || 'root';
+        function binder(fn, level) {
+            return function() {
+                // logging format: Dec 14 2012 15:42:41,453 - LOG - root - [your message here] 
+                var prefix = [timetag(), level, domain].join(' - ') + ' -';
+                fn.apply(console, [prefix].concat([].slice.call(arguments, 0)));
+            };
+        }
+        return {
+            l: binder(console.log, 'LOG'),
+            d: binder(console.debug, 'DEBUG'),
+            i: binder(console.info, 'INFO'),
+            w: binder(console.warn, 'WARNING'),
+            e: binder(console.error, 'ERROR')
+        };
+    }
+
+    var Mono = function() {};
+
+    function defineProperties(o, props) {
+        for (var key in props) {
+            o[key] = props[key];
+        }
+    }
+
+    defineProperties(Mono, {
+        LOG: 1,
+        DEBUG: 2,
+        INFO: 3,
+        WARNING: 4,
+        ERROR: 5
+    });
+
+    // setup default logger
+    defineProperties(Mono, logger());
+
+    return Mono;
+});
